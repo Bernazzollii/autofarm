1174280380a9d5cdfc49202e8e5db77a8421283f
Cesar <cesar@tutto.com.br>
2024-09-17 20:43:02 -0300

    disallow not connected user to make request
    Signed-off-by: Cesar <cesar@tutto.com.br>

--- auth.js
+++ auth.js
@@ -1,13 +1,35 @@
-import { getAvailableReles } from "./db.js";
 import { auth, onAuthStateChanged, provider, signInWithPopup, signOut } from "./firebaseSDK.js";
 
 const signInButton = document.getElementById('signInButton');
 const signOutButton = document.getElementById('signOutButton');
 const message = document.getElementById('message');
+const UserConected = connectUser();
+
+async function connectUser() {
+    new Promise((resolve, reject) => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                signInButton.classList.add('hide');
+                message.classList.remove('hide');
+                message.textContent = `Hello, ${user.displayName}`;
+                signOutButton.classList.remove('hide');
+                resolve(user);
+            } else {
+                message.classList.add('hide');
+                signInButton.classList.remove('hide');
+                reject('User not logged');
+            }
+        });
+    });
+};
 
 const userSignIn = async () => {
     await signInWithPopup(auth, provider).then((result) => {
         const user = result.user;
+        // connectUser();
+        location.reload();
+        setTimeout(() => {
+        }, 1000);
     }).catch((error) => {
         console.log(error);
         const errorCode = error.code;
@@ -23,26 +45,8 @@ const userSignOut = async () => {
     });
 };
 
-const UserConected = new Promise((resolve, reject) => {
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-            signInButton.classList.add('hide');
-            message.classList.remove('hide');
-            message.textContent = `Hello, ${user.displayName}`;
-            signOutButton.classList.remove('hide');
-            getAvailableReles();
-            resolve(user);
-        } else {
-            message.classList.add('hide');
-            signInButton.classList.remove('hide');
-            reject('User not logged');
-        }
-    });
-});
-
-
 signInButton.addEventListener('click', userSignIn);
 signOutButton.addEventListener('click', userSignOut);
 
-export { UserConected };
+export { onAuthStateChanged, UserConected };
 
